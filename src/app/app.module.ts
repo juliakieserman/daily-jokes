23ab32d29e816f3f4e57b59d6b8115cb9f883a96
Julia Kieserman <jbk67@georgetown.edu>
2017-03-03 09:19:56 -0600

    replace with angular2 app using firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { HomePageComponent } from './home-page/home-page.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAq3BR1axTBqeqdqHWbqF68bPShUOiML8Y",
+    authDomain: "jokes-website.firebaseapp.com",
+    databaseURL: "https://jokes-website.firebaseio.com",
+    storageBucket: "jokes-website.appspot.com",
+    messagingSenderId: "1016586563889"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
