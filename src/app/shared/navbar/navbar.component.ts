39f4d7fe871e731c8a0c727f37d3490dd19ff2a7
Julia Kieserman <jk790u@att.com>
2017-03-13 14:28:20 -0500

    start laying out home page with project clarity

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JokesService } from '../../services/jokes.service';
+import { JokeObj } from '../../joke-model';
 
 @Component({
     selector: 'navbar',
     templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.css']
+    styleUrls: ['./navbar.component.css'],
+    providers: [JokesService]
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+    private jokes: JokeObj[] = [];
+    private jokeCount: number = 0;
+    
+    constructor(private jokeService: JokesService) {}
+
+    ngOnInit() {
+        this.getJokes();
+    }
+
+    private getJokes() {
+    this.jokeService.getJokes().subscribe(
+      (jokes) => {
+        jokes.forEach(joke => {
+          this.jokes.push(joke);
+          this.jokeCount++;
+        });
+      });
+    }
 
 }
