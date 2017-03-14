7a2556650a65375d66d75422229786dc24bc65f4
Julia Kieserman <jk790u@att.com>
2017-03-14 07:43:19 -0500

    store email data using hash value

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { EmailObj } from '../email-model';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+import {Md5} from 'ts-md5/dist/md5';
 
 @Injectable()
 export class EmailService {
     private _af;
-    emails: FirebaseListObservable<any[]>;
+    email: FirebaseObjectObservable<any[]>;
 
     constructor(af: AngularFire) {
         this._af = af;
     }
 
-    public addEmail(email: EmailObj) {
-        this.emails = this._af.database.list('/emails');
-        this.emails.push({email});
+    public addEmail(email: EmailObj, hash: any) {
+        const databaseObj = this._af.database.object('/emails');
+        databaseObj.update({ [hash]: email});
     }
 }
--- src/app/subscription-page/subscription-page.component.ts
+++ src/app/subscription-page/subscription-page.component.ts
@@ -1,45 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire } from 'angularfire2';
-import { LoginService } from '../services/login.service';
+//import { LoginService } from '../services/login.service';
 import { Router } from '@angular/router';
 import { EmailService } from '../services/email.service';
 import { EmailObj } from '../email-model';
+import { Md5 } from 'ts-md5/dist/md5';
+
 
 @Component({
   selector: 'app-subscription-page',
   templateUrl: './subscription-page.component.html',
   styleUrls: ['./subscription-page.component.css'],
-  providers: [LoginService, EmailService]
+  providers: [EmailService, Md5]
 })
 export class SubscriptionPageComponent implements OnInit {
 
   private _af: AngularFire;
-  private loginService: LoginService;
+  //private loginService: LoginService;
   private emailService: EmailService;
   private loggedIn: boolean = false;
   private email: EmailObj;
 
   constructor(af: AngularFire, 
-  loginService: LoginService, 
+  //loginService: LoginService, 
   private router: Router,
-  emailService: EmailService) {
+  emailService: EmailService,
+  private _md5: Md5) {
     this._af = af;
-    this.loginService = loginService;
+    //this.loginService = loginService;
     this.emailService = emailService;
    }
 
   ngOnInit() {
-    this.loggedIn = this.loginService.checkLogin();
+    //this.loggedIn = this.loginService.checkLogin();
     this.email = new EmailObj();
   }
 
   appLogin() {
-    this.loginService.login();
+    //this.loginService.login();
   }
 
   subscribe() {
-    console.log(this.email);
-    this.emailService.addEmail(this.email);
+    let hash = Md5.hashStr(this.email.email);
+    this.emailService.addEmail(this.email, hash);
     this.router.navigate(['/home']);
   }
 
