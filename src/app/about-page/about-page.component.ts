df2b930da5c7ede72fe6113745b9ce77315b8ab1
Julia Kieserman <jbk67@georgetown.edu>
2017-03-19 08:51:26 -0500

    add content to about page

---
+++ src/app/about-page/about-page.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { JokesService } from '../services/jokes.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-about-page',
+  templateUrl: './about-page.component.html',
+  styleUrls: ['./about-page.component.css'],
+  providers: [JokesService]
+})
+export class AboutPageComponent implements OnInit {
+
+  constructor(private jokeService: JokesService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  private randomJoke() {
+    let startDate = this.jokeService.getStartDate();
+    const endDate = new Date();
+    let randomDate = new Date(startDate.getTime() + Math.random() * (endDate.getTime() - startDate.getTime()));
+    let param = randomDate.getFullYear() + '-' + this.addZero(randomDate.getDay()) + '-' + this.addZero(randomDate.getMonth());
+    this.router.navigate(['/home', param]);
+  }
+
+  private addZero(value: Number) {
+    let paddedValue;
+    if (value < 10) {
+      paddedValue = '0' + value;
+    }
+    
+    return paddedValue;
+  }
+
+}
--- src/app/services/jokes.service.ts
+++ src/app/services/jokes.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from '@angular/core';
 import { JokeObj } from '../models/joke-model';
 import { AngularFire, FirebaseListObservable, FirebaseRef, FirebaseObjectObservable } from 'angularfire2';
 
+const MONTH_OBJ = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const FIVE = 5;
+const START_TIME = new Date('February 15, 2017 5:08:00');
+const DAY_IN_MILLISECONDS = 1000*60*60*24;
+const DAYS_IN_WEEK = 7;
+const WEEKS_IN_MONTH = 4;
+
 @Injectable()
 export class JokesService {
-    
     private _af;
     public jokeCount: number = 0;
+    
 
     constructor(af: AngularFire) {
         this._af = af;
@@ -18,6 +25,38 @@ export class JokesService {
 
     public getDailyJoke(today: String) {
         return this._af.database.object('/jokes/' + today);
+    }
+
+    public getLastFive(): FirebaseListObservable<any[]> {
+        return this._af.database.list('/jokes', {
+            query: {
+                limitToLast: FIVE
+            }
+        });
+    }
+
+    /* Date functions */
+    public calcDateDiff() {
+        let today = new Date();
+        let todayTime = today.getTime();
+        let startTime = START_TIME.getTime();
+
+        let diff = Math.abs(todayTime - startTime);
+        return Math.round(diff/DAY_IN_MILLISECONDS);
+    }
+
+    public calcWeekDiff() {
+        let days = this.calcDateDiff();
+        return Math.round(days/DAYS_IN_WEEK);
+    }
+
+    public calcMonthDiff() {
+        let weeks = this.calcMonthDiff();
+        return Math.round(weeks/WEEKS_IN_MONTH);
+    }
+
+    public getStartDate() {
+        return START_TIME;
     }
 
     public submitRating(today: Date, updatedArray: number[]) {
